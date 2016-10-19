93c591472d4d78f968a039ab6f6c620187bb32c2
Guillaume Martres <smarter@ubuntu.com>
2016-10-19 20:01:39 +0200

    Arguments from .ensime, references, trees in symbols

--- compiler/src/dotty/tools/dotc/core/Symbols.scala
+++ compiler/src/dotty/tools/dotc/core/Symbols.scala
@@ -384,7 +384,9 @@ object Symbols {
 
     type ThisName <: Name
 
-    //assert(id != 4285)
+    //assert(id != 3548)
+
+    var tree: Tree = _
 
     /** The last denotation of this symbol */
     private[this] var lastDenot: SymDenotation = _
