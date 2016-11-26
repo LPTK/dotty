2be9e91cc5d0c0670d8c793e2778a8b9ba070aee
Guillaume Martres <smarter@ubuntu.com>
2016-11-26 21:55:36 +0100

    typedIdent: avoid other runIds but allow root
    Otherwise can't import from _root_ or <empty>

--- compiler/src/dotty/tools/dotc/typer/Typer.scala
+++ compiler/src/dotty/tools/dotc/typer/Typer.scala
@@ -257,9 +257,10 @@ class Typer extends Namer with TypeAssigner with Applications with Implicits wit
         (prevPrec < prec || prevPrec == prec && (prevCtx.scope eq ctx.scope))
 
       @tailrec def loop(implicit ctx: Context): Type = {
-        if (ctx.scope == null || ctx.outer.runId != ctx.runId) previous
+        if (ctx.scope == null) previous
         else {
-          val outer = ctx.outer
+          var outer = ctx.outer
+          while (outer.runId != ctx.runId && outer.runId != 0) outer = outer.outer
           var result: Type = NoType
 
           // find definition
