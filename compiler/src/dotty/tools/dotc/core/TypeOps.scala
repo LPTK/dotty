78de86a344e8bea405b35f5a5c530962318cfcbe
Guillaume Martres <smarter@ubuntu.com>
2017-01-01 00:12:50 +0100

    Fix exception when calling makePackageObjPrefixExplicit with the root package

--- compiler/src/dotty/tools/dotc/core/TypeOps.scala
+++ compiler/src/dotty/tools/dotc/core/TypeOps.scala
@@ -341,11 +341,14 @@ trait TypeOps { this: Context => // TODO: Make standalone object.
       case _ =>
         tpe
     }
-    tpe.prefix match {
-      case pre: ThisType if pre.cls is Package => tryInsert(pre.cls)
-      case pre: TermRef if pre.symbol is Package => tryInsert(pre.symbol.moduleClass)
-      case _ => tpe
-    }
+    if (tpe.symbol.isRoot)
+      tpe
+    else
+      tpe.prefix match {
+        case pre: ThisType if pre.cls is Package => tryInsert(pre.cls)
+        case pre: TermRef if pre.symbol is Package => tryInsert(pre.symbol.moduleClass)
+        case _ => tpe
+      }
   }
 
   /** If we have member definitions
